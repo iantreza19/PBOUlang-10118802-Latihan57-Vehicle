2371e6865dd53b72683eeb9d5c9a819c8d62a9fc
iantreza19 <55836534+iantreza19@users.noreply.github.com>
2019-11-07 23:07:18 +0700

    Add files via upload

---
+++ PBOUlang10118802Latihan57/src/Bicycle.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IantReza
+ */
+public class Bicycle extends Vehicle {
+     private int myGearCount;
+
+    public Bicycle() {
+    System.out.println("Bicycle");
+    super.setBrand("Trek Bike");
+    System.out.println("Brand : " + super.getBrand());
+    super.setModel("7.4Fx");
+    System.out.println("Model : " + super.getModel());
+    setGearCount(23);
+    System.out.println("Jumlah Gear : " + getGearCount());
+    System.out.println("");
+    }
+
+    public int getGearCount() {
+        return myGearCount;
+    }
+
+    public void setGearCount(int gearCount) {
+        this.myGearCount = gearCount;
+    }
+}
---
+++ PBOUlang10118802Latihan57/src/Vehicle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IantReza
+ */
+public class Vehicle {
+    private String myBrand;    
+    private String myModel;
+
+    public Vehicle() {
+    }
+
+    public String getBrand() {
+        return myBrand;
+    }
+
+    public void setBrand(String brand) {
+        this.myBrand = brand;
+    }
+
+    public String getModel() {
+        return myModel;
+    }
+
+    public void setModel(String model) {
+        this.myModel = model;
+    }
+}
